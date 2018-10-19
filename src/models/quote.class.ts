839dfdd1a3b29c9264aeffb4c2553e880dccc383
Kanogaelias <kanogae@gmail.com>
2018-10-19 14:57:20 +0300

    initial commit

---
+++ src/models/quote.class.ts
@@ -0,0 +1,11 @@
+export class Quote {
+      public quote: string;
+      public author: string;
+
+      constructor(quote: string,author: string){
+     this.quote = quote;
+     this.author = author;
+     
+     
+      }
+}
