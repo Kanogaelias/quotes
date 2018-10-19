ee9591f63817dff23139bb10dbfc005e8b056559
Kanogaelias <kanogae@gmail.com>
2018-10-19 15:29:35 +0300

    add import https

--- src/services/quote.service.ts
+++ src/services/quote.service.ts
@@ -1,1 +1,14 @@
- import {injectable}
+ import { Injectable } from '@angular/core';
+ import { HttpClient } from '@angular/common/http';
+ 
+ @Injectable()
+ export class QuoteService {
+     constructor(public http: HttpClient){
+
+     }
+
+     public getRandomQUote(){
+         return this.http.get('https://http://localhost:4200/')
+         
+     }
+ }
