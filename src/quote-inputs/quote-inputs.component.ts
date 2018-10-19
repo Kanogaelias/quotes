2f1301333fea6d233ecb4ae0116a1684f9537188
Kanogaelias <kanogae@gmail.com>
2018-10-19 16:12:04 +0300

    add call styles from component.scss

--- src/quote-inputs/quote-inputs.component.ts
+++ src/quote-inputs/quote-inputs.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'quote-inputs',
     templateUrl: './quote-inputs.component.html',
-    
+    styleUrls: ['./quote-inputs.component.scss']
 
 })
