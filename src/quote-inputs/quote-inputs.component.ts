d33bcd70b98a5ea48edf572f346611386b84dd45
Kanogaelias <kanogae@gmail.com>
2018-10-19 17:03:24 +0300

    add export class in quotes-input.component.ts

--- src/quote-inputs/quote-inputs.component.ts
+++ src/quote-inputs/quote-inputs.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-
+import { QuoteService } from '../services/quote.service'
+import { Quote } from '../models/quote.class';
 @Component({
     selector: 'quote-inputs',
     templateUrl: './quote-inputs.component.html',
     styleUrls: ['./quote-inputs.component.scss']
 
 })
+export class QuoteInputsCOmponent{
+    public quote  =  new Quote('', '')
+    constructor(public quoteService: QuoteService){
+      this.quoteService.getRandomQUote().subscribe((data: Quote) => {
+        this.quote = new Quote(data.quote, data.author)    
+      });
+    }
+}
